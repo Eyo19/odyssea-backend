8ed5e5f9d13afcc0c7b4a4501f51faea73e8ba9d
Eyo19 <yohann@yes-experience.net>
2026-01-07 19:26:42 +0100

    api-gemini.js

--- netlify/functions/api-gemini.js
+++ netlify/functions/api-gemini.js
@@ -7,7 +7,7 @@ export const handler = async (event) => {
 
   // On appelle l'API Google Gemini
   const apiKey = process.env.GEMINI_API_KEY; // La clé sera stockée chez Netlify
-  const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${apiKey}`;
+const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${apiKey}`;
 
   try {
     const response = await fetch(url, {
